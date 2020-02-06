5ef9b1013b3ad627ca969d3c7e8397b4fbf1d0eb
David Courtaigne <dcourtaigne@gmail.com>
2020-02-06 13:54:02 +0100

    corrected folder structure for AWS layer upload

---
+++ helpers/node_modules/@teknowlogygroup/node-helpers/helpers.js
@@ -0,0 +1,7 @@
+module.exports = {
+    handle:(promise) => {
+        return promise
+            .then(data => ([data, undefined]))
+            .catch(error => Promise.resolve([undefined, error]));
+        }
+}
