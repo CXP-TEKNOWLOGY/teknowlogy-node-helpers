025c6f1d03dbf3e7d9e282ec30d013bac92f038b
David Courtaigne <dcourtaigne@gmail.com>
2020-02-06 13:50:29 +0100

    modified folder structure for AWS Layer deployment

--- helpers.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    handle:(promise) => {
-        return promise
-            .then(data => ([data, undefined]))
-            .catch(error => Promise.resolve([undefined, error]));
-        }
-}
